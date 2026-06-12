91375131fd9510866926e318090ef0477a3e6e53
GuanDD123 <guandongsika@gmail.com>
2026-06-12 09:07:12 +0800

    feat: thumb autosize

--- static/listManager.js
+++ static/listManager.js
@@ -49,6 +49,7 @@ function renderList(data) {
         if (state.viewMode === "grid") {
             const thumbWrap = document.createElement("div");
             thumbWrap.className = "grid-thumb-wrap";
+            thumbWrap.style.aspectRatio = "1 / 1";
             const img = document.createElement("img");
             img.className = "grid-thumb";
             img.src = `data:image/svg+xml;utf8,<svg viewBox="0 0 24 24" fill="%23ffd700" xmlns="http://www.w3.org/2000/svg"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>`;
@@ -78,6 +79,7 @@ function renderList(data) {
             if (state.viewMode === "grid") {
                 const thumbWrap = document.createElement("div");
                 thumbWrap.className = "grid-thumb-wrap";
+                thumbWrap.style.aspectRatio = "1 / 1";
                 const img = document.createElement("img");
                 img.className = "grid-thumb";
                 img.src = `data:image/svg+xml;utf8,<svg viewBox="0 0 24 24" fill="%23ffd700" xmlns="http://www.w3.org/2000/svg"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg>`;
@@ -128,6 +130,13 @@ function renderList(data) {
             if (state.viewMode === "grid") {
                 const thumbWrap = document.createElement("div");
                 thumbWrap.className = "grid-thumb-wrap";
+                if (item.width && item.height) {
+                    thumbWrap.style.aspectRatio = `${item.width} / ${item.height}`;
+                } else if (item.type === "video") {
+                    thumbWrap.style.aspectRatio = "16 / 9";
+                } else {
+                    thumbWrap.style.aspectRatio = "1 / 1";
+                }
                 const img = document.createElement("img");
                 img.className = "grid-thumb";
                 img.loading = "lazy";
