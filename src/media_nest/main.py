e30819ebec2df7577904fc27f1efb826f85d1150
GuanDD123 <guandongsika@gmail.com>
2026-06-16 09:49:29 +0800

    1

--- src/media_nest/main.py
+++ src/media_nest/main.py
@@ -48,4 +48,4 @@ async def index():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("media_nest.main:app", host="0.0.0.0", port=8000, reload=True)
